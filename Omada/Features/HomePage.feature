e4bc1629f332373167875625f45e83bfb2d7d857
molobry <41156614+molobry@users.noreply.github.com>
2018-07-24 15:51:31 +0200

    Add files via upload

--- Omada/Features/HomePage.feature
+++ Omada/Features/HomePage.feature
@@ -10,8 +10,7 @@ Scenario: End to end test cases
 	And I want to check if footer is displayed
 	When I want to search for 'gartner'
 	Then I want to check if there is more then 6 results
-	#I don't understand that step, it is not clear to me
-	#And I want to check that there is There is Safety in Numbers among those
+	#And I want to check that there is 'There is Safety in Numbers' among those results
 	When I want to click 'Gartner IAM Summit 2016 - London' link
 	Then I want to check if I am redirected to page with 'Gartner IAM Summit 2016 - London' title
 	When I navigate to News
@@ -19,3 +18,44 @@ Scenario: End to end test cases
 	And I want to check if there is 'Gartner IAM Summit 2016 - London' news
 	When I have open home test page
 	And I want to click Contact
+	Then I want to click U.S West
+	And I want to mouseover Germany
+
+Scenario: Search phrase
+	When I have open home test page
+	Then I want check if header elements are displayed
+	And I want to check if footer is displayed
+	When I want to search for 'gartner'
+	Then I want to check that there is 'worldwide companies' among those results
+
+
+
+Scenario: Privacy Policy
+	When I have open home test page
+	When I want to click Contact
+	Then I open privacy policy in new tab
+	When I want to close tab
+	When I want to close privacy policy
+	Then I want to check if privacy policy is no longer visible
+
+Scenario: Download PDF file
+	When I have open home test page
+	When I want to close privacy policy
+	And I click Cases from Resources
+	And I click download for 'ecco' company
+	And I fill all require fields
+	| JobTitle | FirstName | LastName | Email               | BusinessPhone | CompanyName | Country |
+	| Tester   | Michal    | Test     | local@localhost.com | 12345678      | Test        | Poland  |
+	And I click accept
+	And I Unlock form by sliding slider
+	And I click download button
+	And I click Download Customer Case
+	And I wait till download of file 'Omada-Case-ECCO-Shoes.pdf' finish
+
+Scenario: Close multiple tabs
+	When I have open home test page
+	When I want to open multiple tabs
+	When I want to close tab
+
+
+	
--- Omada/Features/HomePage.feature.cs
+++ Omada/Features/HomePage.feature.cs
@@ -83,21 +83,134 @@ this.ScenarioSetup(scenarioInfo);
  testRunner.When("I want to search for \'gartner\'", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line 12
  testRunner.Then("I want to check if there is more then 6 results", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
-#line 15
+#line 14
  testRunner.When("I want to click \'Gartner IAM Summit 2016 - London\' link", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line 16
+#line 15
  testRunner.Then("I want to check if I am redirected to page with \'Gartner IAM Summit 2016 - London" +
                     "\' title", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
-#line 17
+#line 16
  testRunner.When("I navigate to News", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line 18
+#line 17
  testRunner.Then("I want to check if I am redirected to page\'Omada News\' page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
-#line 19
+#line 18
  testRunner.And("I want to check if there is \'Gartner IAM Summit 2016 - London\' news", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 20
+#line 19
  testRunner.When("I have open home test page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line 21
+#line 20
  testRunner.And("I want to click Contact", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 21
+ testRunner.Then("I want to click U.S West", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 22
+ testRunner.And("I want to mouseover Germany", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Search phrase")]
+        public virtual void SearchPhrase()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Search phrase", ((string[])(null)));
+#line 24
+this.ScenarioSetup(scenarioInfo);
+#line 25
+ testRunner.When("I have open home test page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 26
+ testRunner.Then("I want check if header elements are displayed", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 27
+ testRunner.And("I want to check if footer is displayed", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 28
+ testRunner.When("I want to search for \'gartner\'", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 29
+ testRunner.Then("I want to check that there is \'worldwide companies\' among those results", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Privacy Policy")]
+        public virtual void PrivacyPolicy()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Privacy Policy", ((string[])(null)));
+#line 33
+this.ScenarioSetup(scenarioInfo);
+#line 34
+ testRunner.When("I have open home test page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 35
+ testRunner.When("I want to click Contact", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 36
+ testRunner.Then("I open privacy policy in new tab", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 37
+ testRunner.When("I want to close tab", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 38
+ testRunner.When("I want to close privacy policy", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 39
+ testRunner.Then("I want to check if privacy policy is no longer visible", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Download PDF file")]
+        public virtual void DownloadPDFFile()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Download PDF file", ((string[])(null)));
+#line 41
+this.ScenarioSetup(scenarioInfo);
+#line 42
+ testRunner.When("I have open home test page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 43
+ testRunner.When("I want to close privacy policy", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 44
+ testRunner.And("I click Cases from Resources", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 45
+ testRunner.And("I click download for \'ecco\' company", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+            TechTalk.SpecFlow.Table table1 = new TechTalk.SpecFlow.Table(new string[] {
+                        "JobTitle",
+                        "FirstName",
+                        "LastName",
+                        "Email",
+                        "BusinessPhone",
+                        "CompanyName",
+                        "Country"});
+            table1.AddRow(new string[] {
+                        "Tester",
+                        "Michal",
+                        "Test",
+                        "local@localhost.com",
+                        "12345678",
+                        "Test",
+                        "Poland"});
+#line 46
+ testRunner.And("I fill all require fields", ((string)(null)), table1, "And ");
+#line 49
+ testRunner.And("I click accept", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 50
+ testRunner.And("I Unlock form by sliding slider", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 51
+ testRunner.And("I click download button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 52
+ testRunner.And("I click Download Customer Case", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 53
+ testRunner.And("I wait till download of file \'Omada-Case-ECCO-Shoes.pdf\' finish", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Close multiple tabs")]
+        public virtual void CloseMultipleTabs()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Close multiple tabs", ((string[])(null)));
+#line 55
+this.ScenarioSetup(scenarioInfo);
+#line 56
+ testRunner.When("I have open home test page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 57
+ testRunner.When("I want to open multiple tabs", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 58
+ testRunner.When("I want to close tab", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
             this.ScenarioCleanup();
         }
@@ -105,5 +218,3 @@ this.ScenarioSetup(scenarioInfo);
 }
 #pragma warning restore
 #endregion
-
-
