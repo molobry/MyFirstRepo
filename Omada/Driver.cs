d099a85b9fc9c7a2d93da88c2e695477b5c1c98f
molobry <41156614+molobry@users.noreply.github.com>
2018-07-24 15:53:07 +0200

    Add files via upload

---
+++ Omada/Driver.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using Omada.BDDSteps;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Omada
+{
+    [Binding]
+    public static class Driver
+    {
+        public static IWebDriver driver;
+        
+
+
+        [BeforeScenario]
+        public static IWebDriver Setup()
+        {
+            
+
+            string browser = ConfigurationManager.AppSettings["Browser"];
+            switch (browser)
+            {
+                case "Chrome":
+                    RunChrome();
+                    break;
+
+                case "IE":
+                    RunIE();
+                    break;
+
+                default:
+                    Console.WriteLine("Browser was not recognizate as IE or Chrome Driver");
+                    Log.log.Info("Browser was not recognizate as IE or Chrome Driver");
+                    break;
+
+            }
+
+            driver.Manage().Window.Maximize();
+
+            return driver;
+
+        }
+        [AfterScenario]
+        public static void Close()
+        {
+            driver.Close();
+            Log.log.Info("Closing driver");
+        }
+
+        public static void RunIE()
+        {
+            driver = new InternetExplorerDriver();
+            Log.log.Info("IE driver running");
+        }
+        public static void RunChrome()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddAdditionalCapability("useAutomationExtension", false);
+            options.AddArgument("--incognito");
+            driver = new ChromeDriver(options);
+            Log.log.Info("Chrome driver running");
+
+        }
+
+
+    }
+}
---
+++ Omada/Tools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omada
+{
+    class Tools
+    {
+        public static void DeleteFiles(string path, string fileContainsName )
+        {
+            string[] Files = Directory.GetFiles(path);
+
+            foreach (string file in Files)
+            {
+                if (file.ToUpper().Contains(fileContainsName.ToUpper()))
+                {
+                    File.Delete(file);
+                }
+            }
+
+        }
+    }
+}
